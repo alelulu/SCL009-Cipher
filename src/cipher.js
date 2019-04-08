83bd52afae246cc64811abcb60f5185acd6c27a4
alelulu <ale.silvasuazo@gmail.com>
2019-04-08 00:22:57 -0400

    Problema con el test

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,36 @@
 window.cipher = {
   encode: (userText, offSet) => {
-    let newText = ""
-    let newCharacter = ""
+    let newText = "";
+    let newCharacter = "";
     for (let i = 0; i < userText.length; i++) {
       let character = userText[i].charCodeAt(0);
       if (character >= 65 && character <= 90){
-        newCharacter = (character - 65 + offSet)%26+65
-        newText = newText.concat(String.fromCharCode(newCharacter))
-      }
-      else if (character >= 97 && character <= 122){
-        newCharacter = (character - 97 + offSet)%26+97
-        newText = newText.concat(String.fromCharCode(newCharacter))
-      }
-      else {
-        newText = newText.concat(userText[i])
+        newCharacter = (character - 65 + offSet) % 26 + 65;
+        newText = newText.concat(String.fromCharCode(newCharacter));
+      } else if (character >= 97 && character <= 122){
+        newCharacter = (character - 97 + offSet) % 26 + 97;
+        newText = newText.concat(String.fromCharCode(newCharacter));
+      } else {
+        newText = newText.concat(userText[i]);
       }
     }
-    return newText
-},
+    return newText;
+  },
   decode: (userText, offSet) => {
-    let newText = ""
-    let newCharacter = ""
+    let newText = "";
+    let newCharacter = "";
     for (let i = 0; i < userText.length; i++) {
       let character = userText[i].charCodeAt(0);
       if (character >= 65 && character <= 90){
-        newCharacter = (character - 90 - offSet)%26+90
-        newText = newText.concat(String.fromCharCode(newCharacter))
+        newCharacter = (character - 90 - offSet) % 26 + 90;
+        newText = newText.concat(String.fromCharCode(newCharacter));
+      } else if (character >= 97 && character <= 122) {
+        newCharacter = (character - 122 - offSet) % 26 + 122;
+        newText = newText.concat(String.fromCharCode(newCharacter));
+      } else {
+        newText = newText.concat(userText[i]);
       }
-      else if (character >= 97 && character <= 122) {
-        newCharacter = (character - 122 - offSet)%26+122
-        newText = newText.concat(String.fromCharCode(newCharacter))
-      }
-      else {
-        newText = newText.concat(userText[i])
-      }
+    }
+    return newText;
   }
-  return newText
-}
 };
